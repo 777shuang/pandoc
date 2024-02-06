07193eb81f887dd8a6a41ef43fcd8f5bf3dbce54
777shuang <shuang777@outlook.jp>
2024-02-06 23:13:14 +0900

    画像ファイルを読み込むため、カレントディレクトリを変更

--- manage.py
+++ manage.py
@@ -15,21 +15,26 @@ dirs = {} # key: ディレクトリ名, value: {key: ファイル名, value: 変
 for dir in os.listdir(os.getcwd()):
     if dir != '.git' and dir != '.github' and os.path.isdir(dir): # ディレクトリかつ、「.git」「.github」でない
         print('found: ' + dir)
+        os.chdir(dir)
         files = {} # key: ファイル名, value: 変更時刻
-        for file in glob(os.path.join(dir, "*.md")): # Markdownファイルについてループ
+        for file in glob('*.md'): # Markdownファイルについてループ
             if file != dir + '.md': # 出力ファイルは除外
-                print('  found: ' + file)
                 files[file] = get_modified_time(file)
-        dirs[dir] = files
+        os.chdir('..')
+        dirs[dir] = dict((key, value) for key, value in sorted(files.items()))
+        for file in dirs[dir]:
+            print('  found: ' + file)
 
 while True:
     for dir, files in dirs.items():
         do = False # コンパイル実行フラグ
+        os.chdir(dir)
         for file, modified_time in files.items():
             if modified_time != get_modified_time(file): # 更新時刻が一致しない(変更された)
                 do = True
                 files[file] = get_modified_time(file) # 更新時刻の更新
                 print('modified: ' + file)
+        os.chdir('..')
         
         if do: # コンパイル実行フラグが立っていたら実行
             md = dir + '.md' # Markdown(結合済み)のファイル名
@@ -38,7 +43,7 @@ while True:
             # 複数のmdファイルを一つにまとめる
             wp = open(md, 'w', encoding='utf_8')
             for file in files:
-                fp = open(file, 'r', encoding='utf_8')
+                fp = open(os.path.join(dir, file), 'r', encoding='utf_8')
                 wp.write(fp.read())
                 wp.write('\n')
                 fp.close()
@@ -51,10 +56,12 @@ while True:
                 '-f', 'markdown',
                 '-t', 'latex',
                 '--template', 'template.tex',
-                md, '-o', tex
+                md, '-o', os.path.join(dir, tex)
             ])
+            os.chdir(dir)
             run(['uplatex', tex]) # LaTeX(1回目)
             run(['uplatex', tex]) # LaTeX(2回目)
             run(['dvipdfmx', dir]) # dvi -> pdf
+            os.chdir('..')
 
     sleep(1)
