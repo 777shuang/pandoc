366448a6f859845e46007a7c09602eaa5734e5b6
777shuang <shuang777@outlook.jp>
2024-02-08 11:43:59 +0900

    コメントの追加

--- build.py
+++ build.py
@@ -4,12 +4,16 @@ from glob import glob
 from sys import argv
 
 def pandoc(dir: str):
+    """Pandocの実行(.md -> .tex)"""
+
     command = ['pandoc', '-sN', '-f', 'markdown', '-t', 'latex', '--template', 'template.tex']
     command += sorted(glob(os.path.join(dir, '*.md')))
     command += ['-o', os.path.join(dir, dir + '.tex')]
     run(command)
 
 def lualatex(dir: str):
+    """LuaLaTeXの実行"""
+
     os.chdir(dir)
     run(['lualatex', '-shell-escape', dir + '.tex'])
     os.chdir('..')
