5a86f08fd98b71c47104d7da1be3336b7b57ed0d
777shuang <shuang777@outlook.jp>
2024-02-08 11:40:27 +0900

    Markdownの結合処理をなくす

--- build.py
+++ build.py
@@ -6,27 +6,13 @@ from sys import argv
 def build(dir: str):
     """ディレクトリをビルド"""
 
-    md = dir + '.md' # Markdown(結合済み)のファイル名
     tex = dir + '.tex' # LaTeXのファイル名
 
-    # 複数のmdファイルを一つにまとめる
-    wp = open(md, 'w', encoding='utf_8')
-    for file in sorted(glob(os.path.join(dir, '*.md'))):
-        fp = open(file, 'r', encoding='utf_8')
-        wp.write(fp.read())
-        wp.write('\n')
-        fp.close()
-    wp.close()
-            
     # pandocの呼び出し(.md -> .tex)
-    run([
-        'pandoc',
-        '-sN',
-        '-f', 'markdown',
-        '-t', 'latex',
-        '--template', 'template.tex',
-        md, '-o', os.path.join(dir, tex)
-    ])
+    command = ['pandoc', '-sN', '-f', 'markdown', '-t', 'latex', '--template', 'template.tex']
+    command += sorted(glob(os.path.join(dir, '*.md')))
+    command += ['-o', os.path.join(dir, tex)]
+    run(command)
     os.chdir(dir)
     run(['lualatex', '-shell-escape', tex])
     os.chdir('..')
