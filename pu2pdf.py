ebf43676b6371b97ab3e1b15b2bd5d2ca6613494
777shuang <shuang777@outlook.jp>
2024-02-12 00:13:31 +0900

    `.pu`ファイルの読み込みに対応

---
+++ pu2pdf.py
@@ -0,0 +1,13 @@
+from subprocess import run
+from sys import argv
+import os
+
+if len(argv) == 3:
+    cat = 'cat'
+    if os.name == 'nt':
+        cat = 'type'
+    tmp_dir = 'svg-inkscape'
+    os.makedirs(tmp_dir, exist_ok=True)
+    svg = os.path.join(tmp_dir, argv[1] + '.svg')
+    os.system(cat + ' ' + argv[1] + ' | plantuml -charset UTF-8 -tsvg -pipe > ' + svg)
+    os.system('inkscape -z --file=' + svg + ' --export-pdf=' + argv[2])
