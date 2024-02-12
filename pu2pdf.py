b625afe505df51dfcb48df2eb3fdd1a7aab07a59
777shuang <shuang777@outlook.jp>
2024-02-12 11:54:31 +0900

    `basename`を使用

--- pu2pdf.py
+++ pu2pdf.py
@@ -7,6 +7,6 @@ if len(argv) == 3:
         cat = 'type'
     tmp_dir = 'svg-inkscape'
     os.makedirs(tmp_dir, exist_ok=True)
-    svg = os.path.join(tmp_dir, argv[1] + '.svg')
+    svg = os.path.join(tmp_dir, os.path.basename(argv[1]) + '.svg')
     os.system(cat + ' ' + argv[1] + ' | java -jar ' + os.path.join('..', 'plantuml.jar') + ' -charset UTF-8 -tsvg -pipe > ' + svg)
     os.system('inkscape -z --file=' + svg + ' --export-pdf=' + argv[2])
