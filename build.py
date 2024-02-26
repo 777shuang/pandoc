0d257a098d2e20d0781a8320e1249bd18264adc7
777shuang <shuang777@outlook.jp>
2024-02-27 01:10:23 +0900

    自動でラベルを付与しない

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from sys import argv
 def build(dir: str):
     """ディレクトリをビルド"""
 
-    command = ['pandoc', '-sN', '--filter', 'pandoc-crossref', '--lua-filter', 'fenced_div.lua', '-f', 'markdown', '-t', 'latex', '--template', 'template.tex']
+    command = ['pandoc', '-sN', '--filter', 'pandoc-crossref', '--lua-filter', 'fenced_div.lua', '-f', 'markdown-auto_identifiers', '-t', 'latex', '--template', 'template.tex']
     command += sorted(glob(os.path.join(dir, '*.md')))
     tex = dir + '.tex'
     command += ['-o', os.path.join(dir, tex)]
