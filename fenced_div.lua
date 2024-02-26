08614cbdb2308507f8d05d2e7776ca81a0fc974d
777shuang <shuang777@outlook.jp>
2024-02-27 01:10:38 +0900

    `:::`ブロックのみ処理

--- fenced_div.lua
+++ fenced_div.lua
@@ -1,7 +1,9 @@
 function Div(el)
-    -- insert element in front
-    table.insert(el.content, 1, pandoc.RawBlock("latex", "\\begin{" .. el.classes[1] .. "}"))
-    -- insert element at the back
-    table.insert(el.content, pandoc.RawBlock("latex", "\\end{" .. el.classes[1] .. "}"))
+    if #el.classes == 1 then
+        -- insert element in front
+        table.insert(el.content, 1, pandoc.RawBlock("latex", "\\begin{" .. el.classes[1] .. "}"))
+        -- insert element at the back
+        table.insert(el.content, pandoc.RawBlock("latex", "\\end{" .. el.classes[1] .. "}"))
+    end
     return el
 end
