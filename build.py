782074bce8daaf6ecacf3cee8442f17b6f090aea
777shuang <shuang777@outlook.jp>
2024-02-29 17:03:26 +0900

    整形

--- build.py
+++ build.py
@@ -6,12 +6,27 @@ from sys import argv
 def build(dir: str):
     """ディレクトリをビルド"""
 
-    command = ['pandoc', '-sN', '--filter', 'pandoc-crossref', '--lua-filter', 'fenced_div_html.lua', '--mathjax', '-f', 'markdown', '-t', 'html', '--template', 'template.html']
+    command = [
+        'pandoc', '-sN',
+        '--filter', 'pandoc-crossref',
+        '--lua-filter', 'fenced_div_html.lua',
+        '-f', 'markdown', '-t', 'html',
+        '--toc', '--mathjax',
+        '--template', 'template.html'
+    ]
     command += sorted(glob(os.path.join(dir, '*.md')))
     command += ['-o', os.path.join(dir, dir + '.html')]
-    run(command)    
+    run(command)
 
-    command = ['pandoc', '-sN', '--filter', 'pandoc-crossref', '--lua-filter', 'fenced_div_latex.lua', '-f', 'markdown-auto_identifiers', '-t', 'latex', '--template', 'template.tex']
+    command = [
+        'pandoc',
+        '-sN',
+        '--filter', 'pandoc-crossref',
+        '--lua-filter', 'fenced_div_latex.lua',
+        '-f', 'markdown-auto_identifiers', '-t', 'latex',
+        '--top-level-division=chapter',
+        '--template', 'template.tex'
+    ]
     command += sorted(glob(os.path.join(dir, '*.md')))
     tex = dir + '.tex'
     command += ['-o', os.path.join(dir, tex)]
