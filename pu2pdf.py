ad74b1f122d35e5971cd21d7323ae66aa72b09cb
777shuang <shuang777@outlook.jp>
2024-03-02 12:39:19 +0900

    ディレクトリ構造を再現する

--- pu2pdf.py
+++ pu2pdf.py
@@ -5,7 +5,7 @@ if len(argv) == 3:
     cat = 'cat'
     if os.name == 'nt':
         cat = 'type'
-    tmp_dir = 'svg-inkscape'
+    tmp_dir = os.path.join(os.path.dirname(argv[1]), 'svg-inkscape')
     os.makedirs(tmp_dir, exist_ok=True)
     svg = os.path.join(tmp_dir, os.path.basename(argv[1]) + '.svg')
     os.system(cat + ' ' + argv[1] + ' | java -jar ' + os.path.join('..', 'plantuml.jar') + ' -charset UTF-8 -tsvg -pipe > ' + svg)
