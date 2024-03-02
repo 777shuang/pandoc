c7c15668d93a5e4cfeb11c0c7746f4c71c344b0e
777shuang <shuang777@outlook.jp>
2024-03-02 13:10:25 +0900

    ディレクトリ構造を再現

--- plantuml.lua
+++ plantuml.lua
@@ -1,7 +1,21 @@
+local dirname = function(str)
+	if str:match(".-/.-") then
+		local name = string.gsub(str, "(.*/)(.*)", "%1")
+		return name
+	else
+		return ''
+	end
+end
+
+local basename = function(str)
+	local name = string.gsub(str, "(.*/)(.*)", "%2")
+	return name
+end
+
 function Image(el)
     local pu = ".pu"
     if el.src:sub(-#pu) == pu then
-        el.src = "svg-inkscape/" .. el.src .. ".svg"
+        el.src = dirname(el.src) .. "svg-inkscape/" .. basename(el.src) .. ".svg"
     end
     return el
 end
